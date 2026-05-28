d65a5521fab8ce32ef71740309eaf6fc7c539aa0
rdxrio123 <riodwibinafitriono@gmail.com>
2026-05-29 00:11:12 +0700

    feat: add index.php to override SCRIPT_NAME for Vercel API routing compatibility

--- api/index.php
+++ api/index.php
@@ -1,3 +1,5 @@
 <?php
+// Mencegah Laravel memotong prefix "/api" dari URL karena Vercel meletakkan index di folder "api"
+$_SERVER['SCRIPT_NAME'] = '/index.php';
 
 require __DIR__ . '/../public/index.php';
